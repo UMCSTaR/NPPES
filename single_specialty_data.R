60e4d911e22f2b2c0f7d12c4508519e9ca3002e6
xilinc <xilinc@med.umich.edu>
2020-08-14 13:07:43 -0400

    add taxonomy to NPI

--- single_specialty_data.R
+++ single_specialty_data.R
@@ -39,3 +39,14 @@ npi_md_single_spty_gs =  npidata_single %>%
 
 
 save(npi_md_single_spty_gs, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_md_single_spty_gs_2020.rdata")
+
+
+# add taxonomy to NPI
+npi_selecct_vars = data.table::fread("X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_20050523-20200112.csv",
+                                select = c('NPI','Provider Credential Text','Healthcare Provider Taxonomy Code_1','Entity Type Code'))
+
+npi_taxonomy_individual = npi_selecct_vars %>% 
+  filter('Entity Type Code' ==1) %>%  # individual
+  left_join(taxonomy, by = c('Healthcare Provider Taxonomy Code_1' = 'PROVIDER TAXONOMY CODE'))  
+
+data.table::fwrite(npi_taxonomy_individual, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_taxonomy_individual.csv" )  
