e4a4f12e1a92ba71510685c17abe64494dd9b6ec
xilinc <xilinc@med.umich.edu>
2020-08-14 14:55:34 -0400

    use medicare specialty group to define GS inNPPES

--- single_specialty_data.R
+++ single_specialty_data.R
@@ -27,26 +27,23 @@ npidata_md = npidata_select %>%
   filter(str_detect(credential, 'MD'), `NPI Deactivation Date`  == "")
 
 # 2. only keep single specialty surgeons
-npidata_single = npidata_md %>% 
+# 3. filter GS
+npi_md_single_spty_gs = npidata_md %>% 
+  filter(`MEDICARE SPECIALTY CODE` == "02") %>% 
   add_count(NPI) %>% 
   filter(n == 1) %>% 
   select(-n)
-
-# 3. filter GS
-npi_md_single_spty_gs =  npidata_single %>% 
-  filter(str_detect(`MEDICARE PROVIDER/SUPPLIER TYPE DESCRIPTION`, "General Surgery")) %>% 
-  glimpse()
-
 
 save(npi_md_single_spty_gs, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_md_single_spty_gs_2020.rdata")
 
 
 # add taxonomy to NPI
 npi_selecct_vars = data.table::fread("X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_20050523-20200112.csv",
-                                select = c('NPI','Provider Credential Text','Healthcare Provider Taxonomy Code_1','Entity Type Code'))
+                                select = c('NPI','Provider Credential Text','Healthcare Provider Taxonomy Code_1','Entity Type Code', 'NPI Deactivation Date'))
 
 npi_taxonomy_individual = npi_selecct_vars %>% 
-  filter(`Entity Type Code` == 1) %>%  # individual
+  filter(`Entity Type Code` == 1,
+         `NPI Deactivation Date`  == "") %>%  # individual
   left_join(taxonomy, by = c('Healthcare Provider Taxonomy Code_1' = 'PROVIDER TAXONOMY CODE'))  
 
 data.table::fwrite(npi_taxonomy_individual, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_taxonomy_individual.csv" )  
