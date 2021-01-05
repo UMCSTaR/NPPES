b95a89e955093b22fe1bcfa3d1d4462ba91f06a1
xilinc <xilinc@med.umich.edu>
2021-01-05 10:17:56 -0500

    taxonomy
    - General surgery 
    - any surgery

--- clean_NPI_data_2020_Jan.R
+++ clean_NPI_data_2020_Jan.R
@@ -1,12 +1,11 @@
 library(tidyverse)
 library(readr)
-library(tidyext)
 library(data.table)
 
 ## import data 
 npidata_pfile_2020 <- fread("X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_20050523-20200112.csv")
 
-## select 
+## 1. select vars------
 npidata_pfile_2020_selected_var = npidata_pfile_2020 %>% 
   select('NPI',
          `Entity Type Code`,
@@ -28,12 +27,15 @@ npidata_pfile_2020_selected_var = npidata_pfile_2020_selected_var %>%
          middle_name = `Provider Middle Name`,
          suffix = `Provider Name Suffix Text`)
 
-save(npidata_pfile_2020_selected_var, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
+# save(npidata_pfile_2020_selected_var, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
 
-# Active Individual -------
+# 2. Individual -------
 npidata_individual = npidata_pfile_2020_selected_var %>%
   mutate(credential = str_replace_all(`Provider Credential Text`, "[^[:alpha:]]", "")) %>% 
   filter(`Entity Type Code` == 1) 
+
+fwrite(npidata_individual, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_individual.csv")
+
 
 npidata_individual_active = npidata_individual %>% 
   filter(`NPI Deactivation Date` == "", is.na(`NPI Deactivation Reason Code`))
@@ -41,8 +43,8 @@ npidata_individual_active = npidata_individual %>%
 fwrite(npidata_individual_active, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_individual_active.csv")
 
 
-# save MD NPI ----
-load("/Volumes/George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
+# 3. MD NPI ----
+load("X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var.rdata")
 
 npidata_individual = npidata_pfile_2020_selected_var %>%
   mutate(credential = str_replace_all(`Provider Credential Text`, "[^[:alpha:]]", "")) %>% 
@@ -54,4 +56,39 @@ npidata_md = npidata_individual %>%
   filter(str_detect(credential, 'MD'))
 
 fwrite(npidata_md, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npidata_pfile_2020_selected_var_md.csv")
+
+# 4. specialty ------
+# taxonomy data for npi
+taxonomy <- readr::read_csv("CROSSWALK_MEDICARE_PROVIDER_SUPPLIER_to_HEALTHCARE_PROVIDER_TAXONOMY.csv")
+
+taxonomy_gs = taxonomy %>%
+  filter(`MEDICARE SPECIALTY CODE` == "02") 
+
+anyDuplicated(taxonomy_gs$`PROVIDER TAXONOMY CODE`)
+  
+# 4.1. GS surgeons -----
+npi_md_spty_gs = npidata_md %>%
+  filter(`Healthcare Provider Taxonomy Code_1` %in% taxonomy_gs$`PROVIDER TAXONOMY CODE`)
+
+fwrite(npi_md_spty_gs, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_md_spty_gs.csv")
+
+
+# 4.2 Surgery -----
+taxonomy_surg = taxonomy %>% 
+  filter(str_detect(`MEDICARE PROVIDER/SUPPLIER TYPE DESCRIPTION`, 'Surgery'))
+
+npi_md_spty_surgery = npidata_md %>%
+  filter(`Healthcare Provider Taxonomy Code_1` %in% taxonomy_surg$`PROVIDER TAXONOMY CODE`)
+
+fwrite(npi_md_spty_surgery, file = "X:\\George_Surgeon_Projects/Other/NPPES_Data_Dissemination_January_2020/npi_md_spty_surgery.csv")
+
+
+
+
+
+
+
+
+
+
 
